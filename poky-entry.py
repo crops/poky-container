e01b97efb863cf99e7d481164bddd207442823a0
brian avery <brian.avery@intel.com>
2017-01-10 16:03:02 -0800

    poky-entry.py: add cmd argument
    Aimed at testing, this allows us to pass a command to the container and
    have it run that command.  If the command returns a non zero value, so
    does the docker run command itself.
    
    Signed-off-by: brian avery <brian.avery@intel.com>

--- poky-entry.py
+++ poky-entry.py
@@ -28,16 +28,21 @@ parser.add_argument('--workdir', default='/home/pokyuser',
                          'In the abscence of the "id" argument, the uid and '
                          'gid of the workdir will also be used for the user '
                          'in the container.')
+
 parser.add_argument("--id",
                     help='uid and gid to use for the user inside the '
                          'container. It should be in the form uid:gid')
+
+parser.add_argument("--cmd",default='',
+                    help='command to run after setting up container. '
+                         'Often used for testing.')
 
 args = parser.parse_args()
 
 
 idargs = ""
 if args.id:
-    uid, gid = args.id.split(":") 
+    uid, gid = args.id.split(":")
     idargs = "--uid={} --gid={}".format(uid, gid)
 
 elif args.workdir == '/home/pokyuser':
@@ -46,6 +51,7 @@ elif args.workdir == '/home/pokyuser':
     idargs = "--uid=1000 --gid=1000"
 
 cmd = """usersetup.py --username=pokyuser --workdir={wd} {idargs}
-         poky-launch.sh {wd}""".format(wd=args.workdir, idargs=idargs)
+         poky-launch.sh {wd} {cmd}""" \
+             .format(wd=args.workdir, idargs=idargs,cmd=args.cmd)
 cmd = cmd.split()
 os.execvp(cmd[0], cmd)
