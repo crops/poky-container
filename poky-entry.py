20331f732364773a02c2ead1550d2c47a1c3ca8e
brian avery <brian.avery@intel.com>
2017-01-10 16:03:02 -0800

    poky-entry.py: add cmd argument
    Aimed at testing, this allows us to pass a command to the container and
    have it run that command.  If the command returns a non zero value, so
    does the docker run command itself.
    
    Signed-off-by: brian avery <brian.avery@intel.com>

--- poky-entry.py
+++ poky-entry.py
@@ -24,28 +24,34 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--workdir', default='/home/pokyuser',
-                    help='The active directory once the container is running. '
-                         'In the abscence of the "id" argument, the uid and '
-                         'gid of the workdir will also be used for the user '
-                         'in the container.')
+                    help='The active directory once the container is '
+                    'running. In the abscence of the "id" argument, '
+                    ' the uid and gid of the workdir will also be '
+                    ' be used for the user in the container.')
 parser.add_argument("--id",
                     help='uid and gid to use for the user inside the '
-                         'container. It should be in the form uid:gid')
+                    'container. It should be in the form uid:gid')
+
+parser.add_argument("--cmd",default='',
+                    help='command to run after setting up container. '
+                    'Often used for testing.')
 
 args = parser.parse_args()
 
 
 idargs = ""
 if args.id:
-    uid, gid = args.id.split(":") 
+    uid, gid = args.id.split(":")
     idargs = "--uid={} --gid={}".format(uid, gid)
 
 elif args.workdir == '/home/pokyuser':
     # If the workdir wasn't specified pick a default uid and gid since
-    # usersetup won't be able to calculate it from the non-existent workdir
+    # usersetup won't be able to calculate it
+    # from the non-existent workdir
     idargs = "--uid=1000 --gid=1000"
 
 cmd = """usersetup.py --username=pokyuser --workdir={wd} {idargs}
-         poky-launch.sh {wd}""".format(wd=args.workdir, idargs=idargs)
+         poky-launch.sh {wd} {cmd}""" \
+             .format(wd=args.workdir, idargs=idargs,cmd=args.cmd)
 cmd = cmd.split()
 os.execvp(cmd[0], cmd)
