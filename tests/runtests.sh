66bb2672fcec611ba2cc37d3c931645b598edfaa
brian avery <brian.avery@intel.com>
2017-01-12 10:11:32 -0800

    tests/runtests.sh: run set of tests
    This runs tests with the name tests/run-XXX.sh. If they fail, it returns
    a non zero exit code. Takes an argument of what IMAGE to run, for
    example crops/poky:ubuntu-14.04
    
    Signed-off-by: brian avery <brian.avery@intel.com>

---
+++ tests/runtests.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# runtests.sh
+#
+# Copyright (C) 2016 Intel Corporation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+IMAGE=$1
+
+for i in run-*.sh; do
+    echo Running Test $i
+    # make temporary workdir
+    LOCAL_WDIR=$(mktemp -d  wdir_XXXXX)
+    # get absolute path to it
+    LOCAL_WDIR=$(readlink -f ${LOCAL_WDIR})
+    cp $i $LOCAL_WDIR
+    docker run -it --rm  -v $LOCAL_WDIR:/workdir $IMAGE \
+	--workdir=/workdir --cmd="/workdir/$i"
+    RET=$?
+    if [ ${RET} != 0 ]; then
+	echo "Test $i failed"
+	echo "Workdir located in $LOCAL_WDIR"
+	exit ${RET}
+    fi
+    rm $LOCAL_WDIR -rf
+done
+echo "All tests PASSED"
