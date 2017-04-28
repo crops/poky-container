f856f0c8e9dafb370294aa249f97753e0e510ea0
Randy Witt <randy.e.witt@linux.intel.com>
2017-04-28 14:31:03 -0700

    build-and-test.sh: Add build-and-test.sh
    build-and-test.sh does what was previously done only in the .travis.yml.
    It does sed substitution on the Dockerfile to build based on a
    particular distro, as well as runs the test.
    
    That way a user can test things locally just like travis does, since
    travis now will invoke build-and-test.sh.
    
    Signed-off-by: Randy Witt <randy.e.witt@linux.intel.com>

---
+++ build-and-test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Copyright (C) 2016 Intel Corporation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+set -e
+
+DOCKERFILE=`mktemp -p . Dockerfile.XXX`
+
+sed -e "s#FROM crops/yocto:ubuntu-14.04#FROM crops/yocto:${BASE_DISTRO}#" Dockerfile > $DOCKERFILE
+docker build --pull -f $DOCKERFILE -t ${REPO}:${BASE_DISTRO} .
+
+if command -v annotate-output; then
+    ANNOTATE_OUTPUT=annotate-output
+fi
+$ANNOTATE_OUTPUT bash -c "cd tests; ./runtests.sh ${REPO}:${BASE_DISTRO}"
+
+rm -f $DOCKERFILE
