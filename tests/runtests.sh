a2f24d6b75c7084b278b371d62b34f643dcc327d
André Draszik <andre.draszik@jci.com>
2018-08-24 10:06:06 +0100

    poky-entry: make compatible with standard docker invocation
    This docker image has two issues:
        * the command to execute is expected to be passed via
          --cmd to the entrypoint, rather than just the
          remaining arguments as per docker best practices [1]
          when using 'docker run' to start a container
        * it ignores any --workdir passed via the 'docker run'
          command, and instead expects the workdir to be
          passed as an argument to the entrypoint
    
    In other words, a normal/standard 'docker run' command
    line would be e.g (adopted from the runtests.sh cript
    here):
        docker run --rm -v $LOCAL_WDIR:/workdir:Z --workdir=/workdir $IMAGE "/workdir/$i"
    whereas this image wants to be started as
        docker run --rm -v $LOCAL_WDIR:/workdir:Z $IMAGE --workdir=/workdir --cmd="/workdir/$i"
    
    i.e.:
        * --workdir as a docker run argument vs. --workdir as
          a poky-entry.py argument
        * command to execute as CMD (as per Dockerfile spec)
          vs. extra argument --cmd= needed for poky-entry.py
    
    The problem with that approach is:
        * the --workdir argument in poky-entry.py is
          duplicating functionality that already exists in
          plain 'docker run'.
          People are already used to passing --workdir= as
          'docker run' argument, and also no existing generic
          infrastructure knows about this difference, e.g. the
          Jenkins docker-workflow plugin:
               It already passes --workdir as part of
               'docker run', but we have to duplicate this for
               no benefit when using this image
        * as per docker best practices [1], the image should
          be able to run as though it was a command.
          Again, people are already used to that, and also
          existing generic infrastructure expects this to be
          the case, e.g. the Jenkins docker-workflow plugin
          will fail to work otherwise with the following
          message:
              ERROR: The container started but didn't run the expected command.
              Please double check your ENTRYPOINT does execute the command
              passed as docker run argument. See [1] for entrypoint best practices.
    
    To resolve all of the above non-standard behaviours,
    poky-entry.py now:
        * (tries to) check if a --workdir was given in the
          'docker run' arguments, by checking if the current
          directory is different from the image's default
          '/home/yoctouser'
        * accepts additional arguments without a --cmd flag.
          Any remaining arguments now specify the command
          (and its optional arguments) to run
    
    Note that we have to update the tests at the same time so
    as not to break the travis CI build/tests:
        * runtests.sh: update the 'docker run' command as per
                       above
                       allow everybody to 'cd' into the temp
                       directory as docker will execute PID 1
                       from there, and the usersetup user
                       running PID 1 otherwise can't access it
        * run-dumb-init-check.sh: update the expected command
                                  line as per above
                                  add an extra check for cwd
                                  now that this isn't part of
                                  the command line anymore.
    
    [1] https://docs.docker.com/develop/develop-images/dockerfile_best-practices/#entrypoint
    
    Signed-off-by: André Draszik <andre.draszik@jci.com>

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -25,9 +25,12 @@ for i in run-*.sh; do
     LOCAL_WDIR=$(mktemp -d  wdir_XXXXX)
     # get absolute path to it
     LOCAL_WDIR=$(readlink -f ${LOCAL_WDIR})
+    chmod o+x $LOCAL_WDIR
     cp $i $LOCAL_WDIR
-    docker run --rm -t -v $LOCAL_WDIR:/workdir:Z $IMAGE \
-	--workdir=/workdir --cmd="/workdir/$i"
+    docker run --rm -t -v $LOCAL_WDIR:/workdir \
+	--workdir /workdir \
+	$IMAGE \
+	/workdir/$i
     RET=$?
     if [ ${RET} != 0 ]; then
 	echo "Test $i failed"
